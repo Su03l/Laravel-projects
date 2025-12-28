b023f909d0e1f4ed61c5ac6cf1a416679cb5c71f
Suliman <salmanyousef13@gmail.com>
2025-12-28 22:14:43 +0300

    fix errors in model

--- Project_Manager/app/Models/Attachment.php
+++ Project_Manager/app/Models/Attachment.php
@@ -9,4 +9,17 @@ class Attachment extends Model
 {
     /** @use HasFactory<\Database\Factories\AttachmentFactory> */
     use HasFactory;
+    protected $guarded = [];
+
+    protected $appends = ['url'];
+
+    public function attachable()
+    {
+        return $this->morphTo();
+    }
+
+    public function getUrlAttribute()
+    {
+        return asset('storage/' . $this->file_path);
+    }
 }
