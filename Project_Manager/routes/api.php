056eb917d888d2e88f4c4f57494c948f1c14e873
Suliman <salmanyousef13@gmail.com>
2025-12-28 22:11:49 +0300

    fix the Route errors

--- Project_Manager/routes/api.php
+++ Project_Manager/routes/api.php
@@ -6,16 +6,3 @@ use App\Http\Controllers\NoteController;
 use App\Http\Controllers\AttachmentController;
 
 Route::apiResource('projects', ProjectController::class);
-
-Route::apiResource('tasks', TaskController::class);
-
-Route::post('/notes', [NoteController::class, 'store']);
-
-Route::put('/notes/{note}', [NoteController::class, 'update']);
-
-Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
-
-Route::post('/attachments', [AttachmentController::class, 'store']);
-
-Route::delete('/attachments/{attachment}', [AttachmentController::class, 'destroy']);
-
