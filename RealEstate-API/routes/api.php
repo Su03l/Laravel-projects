c6be12636679547f9ba64b754a7cb50b0e5acab9
Suliman <salmanyousef13@gmail.com>
2025-12-29 22:52:21 +0300

    update for Route

--- RealEstate-API/routes/api.php
+++ RealEstate-API/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\PropertyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Search properties
 Route::get('/properties/search', [PropertyController::class, 'search']);
 
+// CRUD operations
 Route::apiResource('properties', PropertyController::class);
