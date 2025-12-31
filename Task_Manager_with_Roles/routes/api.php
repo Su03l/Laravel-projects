a33d59fc072a495c0c90849ab62446c824fa7567
Suliman <salmanyousef13@gmail.com>
2026-01-01 01:18:51 +0300

    update the Route

--- Task_Manager_with_Roles/routes/api.php
+++ Task_Manager_with_Roles/routes/api.php
@@ -18,7 +18,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // CRUD للمهام
+    // Tasks
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::post('/tasks', [TaskController::class, 'store']);
     Route::put('/tasks/{id}', [TaskController::class, 'update']);
