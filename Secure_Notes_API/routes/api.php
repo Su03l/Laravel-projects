8bc5ca6e92cad4c4e62ec206fb4626cdf553ceb5
Suliman <salmanyousef13@gmail.com>
2025-12-31 04:37:20 +0300

    fix the Route API

--- Secure_Notes_API/routes/api.php
+++ Secure_Notes_API/routes/api.php
@@ -7,18 +7,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::get('/users', [UserController::class, 'index']);      // عرض الكل
-    Route::get('/users/{id}', [UserController::class, 'show']);  // عرض واحد
-    Route::put('/users/{id}', [UserController::class, 'update']); // تعديل (محمي)
-    Route::delete('/users/{id}', [UserController::class, 'destroy']); // حذف (محمي)
-
-
-    Route::get('/notes/search', [NoteController::class, 'search']);
-    // النوتات لازم تسجيل دخول
-    Route::apiResource('notes', NoteController::class);
-});
