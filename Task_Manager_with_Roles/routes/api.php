20b12413ed2fee6ec8622143e94abb136e74cc77
Suliman <salmanyousef13@gmail.com>
2026-01-01 01:18:32 +0300

    update the Route

--- Task_Manager_with_Roles/routes/api.php
+++ Task_Manager_with_Roles/routes/api.php
@@ -10,3 +10,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class);
 });
+Route::post('/register', [AuthController::class, 'register']);
+// Login (Public)
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // CRUD للمهام
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+});
