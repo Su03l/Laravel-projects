b49cfef1046dddb63a3988a232aa499591a7c801
Suliman <salmanyousef13@gmail.com>
2026-01-02 04:49:16 +0300

    fix the provider

--- ECommerce-API/app/Providers/AppServiceProvider.php
+++ ECommerce-API/app/Providers/AppServiceProvider.php
@@ -20,12 +20,4 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
-            $openApi->secure(
-                SecurityScheme::http('bearer')
-            );
-        });
-    }
 }
