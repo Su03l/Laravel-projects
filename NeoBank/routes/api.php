d9d38ca87e6445e3b3a416a3467ed7d622d98d1a
Suliman <salmanyousef13@gmail.com>
2026-01-06 12:02:28 +0300

    update api.php

--- NeoBank/routes/api.php
+++ NeoBank/routes/api.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// for Authenticated Routes
 Route::middleware('auth:sanctum')->group(function () {
 
+    // for Authenticated Routes
     Route::post('/logout', [AuthController::class, 'logout']);
+
 
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::put('/profile', [ProfileController::class, 'update']);
