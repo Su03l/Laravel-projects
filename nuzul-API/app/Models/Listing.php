86a01723d25780d98e628e93b4b3becceec6e071
Suliman <salmanyousef13@gmail.com>
2026-01-09 20:29:42 +0300

    some update

--- nuzul-API/app/Models/Listing.php
+++ nuzul-API/app/Models/Listing.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    /** @use HasFactory<\Database\Factories\ListingFactory> */
     use HasFactory;
 
     protected $fillable = [
