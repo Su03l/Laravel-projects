c2cf04866968d2379adc78e3693178db2d8b8242
Suliman Yousef <96296776+Su03l@users.noreply.github.com>
2026-01-12 20:45:15 +0300

    Delete Challange 30 Days/day02-blog directory

--- Challange 30 Days/day02-blog/backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('categories', CategoryController::class);
-
-Route::apiResource('posts', PostController::class);
