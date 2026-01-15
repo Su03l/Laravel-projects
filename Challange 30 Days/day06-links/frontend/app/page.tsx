dcadabf67cebb0d75b5ce231869c9495d2deecf1
Suliman <salmanyousef13@gmail.com>
2026-01-16 00:25:36 +0300

    some update

--- Challange 30 Days/day06-links/frontend/app/page.tsx
+++ Challange 30 Days/day06-links/frontend/app/page.tsx
@@ -48,7 +48,6 @@ export default function Home() {
         </div>
       </header>
 
-      {/* Hero Section */}
       <main className="relative">
         <div className="absolute inset-0 overflow-hidden">
           <div className="absolute left-1/2 top-0 h-[500px] w-[800px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white/5 blur-3xl" />
