31e73fec69f241db58cf314622055e363a6cd615
Suliman <salmanyousef13@gmail.com>
2026-01-16 00:26:14 +0300

    some update

--- Challange 30 Days/day06-links/frontend/app/page.tsx
+++ Challange 30 Days/day06-links/frontend/app/page.tsx
@@ -85,7 +85,6 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Links List */}
           <LinkList refreshTrigger={refreshTrigger} onShowToast={showToast} />
         </div>
       </main>
