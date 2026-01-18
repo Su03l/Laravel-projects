61c807ccdcd6582d2924db251c109a1d1db4ccdd
Suliman <salmanyousef13@gmail.com>
2026-01-19 01:21:55 +0300

    some update

--- Challange 30 Days/day09-wallet/frontend/components/dashboard/TransactionModal.tsx
+++ Challange 30 Days/day09-wallet/frontend/components/dashboard/TransactionModal.tsx
@@ -97,8 +97,6 @@ export default function TransactionModal({
                             {error}
                         </div>
                     )}
-
-                    {/* Title */}
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">
                             العنوان
