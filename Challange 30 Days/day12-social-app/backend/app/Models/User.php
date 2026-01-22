6aee567239d5cb7570f7d6f69ecf6038143db1b7
Suliman <salmanyousef13@gmail.com>
2026-01-22 17:37:12 +0300

    some update

--- Challange 30 Days/day12-social-app/backend/app/Models/User.php
+++ Challange 30 Days/day12-social-app/backend/app/Models/User.php
@@ -18,11 +18,6 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
