1efff4639739b117c5643e6491e397648e6a394a
Suliman <salmanyousef13@gmail.com>
2026-01-25 02:00:37 +0300

    soem update

--- Challange 30 Days/day15-payflow/backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Challange 30 Days/day15-payflow/backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -40,9 +40,7 @@ return new class extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+ 
     public function down(): void
     {
         Schema::dropIfExists('users');
