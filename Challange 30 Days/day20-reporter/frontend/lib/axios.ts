e28ba601e57fc42fb29a493b24938f58f222f186
Suliman <salmanyousef13@gmail.com>
2026-01-31 23:22:12 +0300

    some update

---
+++ Challange 30 Days/day20-reporter/frontend/lib/axios.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+});
+
+api.interceptors.request.use((config) => {
+    if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response && error.response.status === 401) {
+        if (typeof window !== 'undefined') {
+            localStorage.removeItem('token');
+            localStorage.removeItem('role');
+            // Avoid redirect loop if already on login
+            if (window.location.pathname !== '/login') {
+                window.location.href = '/login';
+            }
+        }
+    }
+    return Promise.reject(error);
+});
+
+export default api;
