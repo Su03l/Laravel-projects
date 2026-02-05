d99dca4fe65941cbd1067f6943ea0a67621058d6
Suliman <salmanyousef13@gmail.com>
2026-02-05 21:15:07 +0300

    some update

--- Challange 30 Days/day23-livechat/frontend/next.config.ts
+++ Challange 30 Days/day23-livechat/frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // eslint: { ignoreDuringBuilds: true } // Removed as per error
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
