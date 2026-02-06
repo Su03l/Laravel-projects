1457764d8dc79b8ae0508e2b9289950a627d3f78
Suliman <salmanyousef13@gmail.com>
2026-02-06 22:47:48 +0300

    some update

--- Challange 30 Days/day24-ticket-system/backend/app/Models/User.php
+++ Challange 30 Days/day24-ticket-system/backend/app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
