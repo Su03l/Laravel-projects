ed809a9a6027723bc8586a26d40120082a1e4a36
Suliman <salmanyousef13@gmail.com>
2026-02-06 22:49:00 +0300

    some update

--- Challange 30 Days/day24-ticket-system/backend/database/seeders/DatabaseSeeder.php
+++ Challange 30 Days/day24-ticket-system/backend/database/seeders/DatabaseSeeder.php
@@ -12,9 +12,6 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // 1. استدعاء سيدر الأقسام (ضروري جداً للتذاكر)
