37b83549314361e26d207d1727744fde7db0bb1e
Suliman <salmanyousef13@gmail.com>
2026-02-09 01:57:20 +0300

    some update

---
+++ Challange 30 Days/day26-StayEase/backend/config/cors.php
@@ -0,0 +1,43 @@
+2<?php
+
+    return [
+
+        /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+        'paths' => [
+            'api/*',
+            'sanctum/csrf-cookie',
+            'login',
+            'logout',
+            'register',
+        ],
+
+        'allowed_methods' => ['*'],
+
+        'allowed_origins' => [
+            'http://localhost:3000', // رابط الفرونت إند الخاص بك
+            'http://localhost:5173', // رابط Vite الافتراضي (للاحتياط)
+        ],
+
+        'allowed_origins_patterns' => [],
+
+        'allowed_headers' => ['*'],
+
+        'exposed_headers' => [],
+
+        'max_age' => 0,
+
+        'supports_credentials' => true, 
+
+    ];
