c7781001e7d9f094d694712dcf3932c97307a4fe
Suliman <salmanyousef13@gmail.com>
2026-02-11 01:05:25 +0300

    some update

--- Challange 30 Days/day27-GeniusLab/backend/app/Http/Requests/Ai/GenerateRequest.php
+++ Challange 30 Days/day27-GeniusLab/backend/app/Http/Requests/Ai/GenerateRequest.php
@@ -18,8 +18,7 @@ class GenerateRequest extends FormRequest
             'model_slug' => 'required|exists:ai_models,api_slug',
             'chat_id' => 'nullable|exists:chats,id',
             'image' => 'nullable|image|max:5120', // 5MB Max
-            // نسمح بملفات الكود (حجم أقصى 2MB مثلاً للملفات النصية كافي جداً)
-            'code_file' => 'nullable|file|mimes:php,js,py,html,css,json,sql,java,cpp,txt,ts,tsx,jsx,vue,blade.php|max:2048',
+            'code_file' => 'nullable|file|mimes:php,js,py,html,css,json,sql,java,cpp,txt,ts,tsx,jsx,vue,blade.php|max:10240',
         ];
     }
 }
