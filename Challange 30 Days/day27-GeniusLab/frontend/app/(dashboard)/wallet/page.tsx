549bb40f984a2ee548d9de1a6af37aeda9601717
Suliman <salmanyousef13@gmail.com>
2026-02-13 01:05:24 +0300

    some update

--- Challange 30 Days/day27-GeniusLab/frontend/app/(dashboard)/wallet/page.tsx
+++ Challange 30 Days/day27-GeniusLab/frontend/app/(dashboard)/wallet/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/table"
 import { useAuth } from "@/context/AuthContext"
 
+// interfaces 
 interface Transaction {
     id: number
     type: string
@@ -31,6 +32,7 @@ interface WalletData {
     transactions: Transaction[]
 }
 
+// component
 export default function WalletPage() {
     const { user } = useAuth()
 
