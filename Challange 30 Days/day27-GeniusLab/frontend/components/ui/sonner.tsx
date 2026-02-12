3e771c90134086a90239ffc665d478a13a272907
Suliman <salmanyousef13@gmail.com>
2026-02-13 01:01:49 +0300

    some update

--- Challange 30 Days/day27-GeniusLab/frontend/components/ui/sonner.tsx
+++ Challange 30 Days/day27-GeniusLab/frontend/components/ui/sonner.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
+// Toaster component 
 const Toaster = ({ ...props }: ToasterProps) => {
     return (
         <Sonner
