dba0b2011784e604ab10250aa11c8f64c65c7b92
Suliman <salmanyousef13@gmail.com>
2026-02-14 04:28:31 +0300

    some update

--- Challange 30 Days/day29-orbit/app/Models/Activity.php
+++ Challange 30 Days/day29-orbit/app/Models/Activity.php
@@ -11,11 +11,13 @@ class Activity extends Model
 
     protected $guarded = [];
 
+    // this for get user who did activity
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    // this for get task that did activity
     public function task()
     {
         return $this->belongsTo(Task::class);
