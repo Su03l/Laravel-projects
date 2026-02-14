71a88678c98b8415d1520e6b17d2efa026ca90a0
Suliman <salmanyousef13@gmail.com>
2026-02-14 04:29:03 +0300

    some update

--- Challange 30 Days/day29-orbit/app/Models/Project.php
+++ Challange 30 Days/day29-orbit/app/Models/Project.php
@@ -11,17 +11,20 @@ class Project extends Model
 
     protected $guarded = [];
 
+    // this for get workspace that project belong to
     public function workspace()
     {
         return $this->belongsTo(Workspace::class);
     }
 
+    // this for get columns that project belong to
     public function columns()
     {
         // ترتيب الأعمدة دائماً حسب الـ Position
         return $this->hasMany(TaskColumn::class)->orderBy('position');
     }
 
+    // this for get tasks that project belong to
     public function tasks()
     {
         return $this->hasMany(Task::class);
