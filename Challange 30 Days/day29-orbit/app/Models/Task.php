8e47467053219cbcbd177af4cb809b3dbead6f43
Suliman <salmanyousef13@gmail.com>
2026-02-14 04:29:34 +0300

    some update

--- Challange 30 Days/day29-orbit/app/Models/Task.php
+++ Challange 30 Days/day29-orbit/app/Models/Task.php
@@ -11,21 +11,25 @@ class Task extends Model
 
     protected $guarded = [];
 
+    // this for get column that task belong to
     public function column()
     {
         return $this->belongsTo(TaskColumn::class, 'task_column_id');
     }
 
+    // this for get project that task belong to
     public function project()
     {
         return $this->belongsTo(Project::class);
     }
 
+    // this for get user who assigned to task
     public function assignee()
     {
         return $this->belongsTo(User::class, 'assigned_to');
     }
 
+    // this for get comments that task have
     public function comments()
     {
         return $this->hasMany(Comment::class)->latest();
